b02f5ecbd593b67a8b8a498307ae04d5c6a902d4
WingKill <zpswlsldi@gmail.com>
2024-05-28 17:40:12 +0900

    Predicate<T>는 test 구현하는 것. 스트림에서 자주 쓴 람다식이라고 함

--- java_lamda/src/LamdaTest2.java
+++ java_lamda/src/LamdaTest2.java
@@ -2,6 +2,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
+import java.util.function.Predicate;
 
 class SLenComp implements Comparator<String> {
 
@@ -12,9 +14,77 @@ class SLenComp implements Comparator<String> {
 	
 }
 
+@FunctionalInterface
+interface Calculate{
+	void cal(int a, int b);
+}
+
+@FunctionalInterface
+interface Calculate2<T>{
+	T cal(T a, T b);
+}
+
+
+// 매개변수가 있고 리턴값을 반환하는 람다식
+interface HowLong{
+	int len(String s);
+}
+
+// 매개변수가 없는 람다식
+interface Generator{
+	int rand();
+}
+
 // 람다가 필요한 상황
 public class LamdaTest2 {
 	public static void main(String[] args) {
+		// boolean test(T t); 
+		
+		Predicate<Integer> predicate = (num) -> num < 10;
+		System.out.println(predicate.test(2));		
+		System.out.println("/////////////////////////////////////////");
+		/////////////////////////////////////////
+		Calculate2<Integer> cal = (a,b) -> a+b;
+		
+		System.out.println(cal.cal(1, 2)); 
+		
+		Calculate2<Double> cal2 = (a,b) -> a+b;
+		System.out.println(cal2.cal(1.1, 2.2));
+		
+		/////////////////////////////////////////
+		
+		Generator generator = () -> {
+			Random rand = new Random();
+			return rand.nextInt();
+		};
+		
+		/////////////////////////////////////////
+		HowLong hl = (s) -> {
+			System.out.println(s);
+			return s.length();
+		};
+		
+		//HowLong h2 = (String s) -> {return s.length();};
+		HowLong h2 = (s) -> s.length();
+		int y = h2.len("안녕?");
+		System.out.println("y : " + y);
+		
+		int x = hl.len("안녕하세요");
+		System.out.println(x);
+		/////////////////////////////////////////
+		Calculate calculate;
+		
+		//calculate = (int a, int b) -> {System.out.println(a+b);};
+		calculate = (a,b) -> System.out.println(a+b);
+		calculate.cal(1, 3);
+		
+		calculate = (a,b) -> System.out.println(a-b);
+		calculate.cal(1, 3);
+		
+		calculate = (a,b) -> System.out.println(a*b);
+		calculate.cal(1, 3);
+		
+		/////////////////////////////////////////
 		List<String> list = new ArrayList<String>();
 		list.add("Robot");
 		list.add("Lambda");
